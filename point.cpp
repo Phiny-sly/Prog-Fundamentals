6373edcd34cf1d22e758c9944bf65742a3b28a0b
Phiny-sly <phinehasanteh2@gmail.com>
2021-09-27 12:44:45 +0000

    New file

---
+++ point.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+	{
+		string input[]; 
+		
+		
+
+		cout<<"Type a sentence";
+		cin>>input;
+
+	
+	
+
+	       for(int i=0; i<5;i++)
+	       {
+		       cout<<input[i]<<endl;
+	       }
+	}
+
+
+
+
+
+		
+		
+	
---
+++ trial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+	{
+		string sent;
+		cout<<"Type a sentence: ";
+		getline( cin, sent,'-');
+
+		cout<<sent;
+
+	}
